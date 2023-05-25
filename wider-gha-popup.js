593918e7fb3ebf2ef9564fd1db21fded2c3c3c9e
Cheng Huang <chuang@mdsol.com>
2023-05-25 19:18:04 -0400

    Create wider-gha-popup.js

---
+++ wider-gha-popup.js
@@ -0,0 +1,35 @@
+// ==UserScript==
+// @name        Wider GitHub Actions Workflow Popup
+// @version     0.0.1
+// @description A userscript that makes Github Actions "Run Workflow" window wider
+// @license     MIT
+// @namespace   https://github.com/chenghuang-mdsol
+// @run-at      document-end
+// @author      Cheng Huang
+// @match       https://github.com/*/actions/workflows/*
+// @grant       GM_addStyle
+// @grant       GM_getValue
+// @grant       GM_setValue
+// @grant       GM_registerMenuCommand
+// @require     https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.4/moment-with-locales.min.js
+// @require     https://greasyfork.org/scripts/28721-mutations/code/mutations.js?version=1108163
+// @require     https://greasyfork.org/scripts/398877-utils-js/code/utilsjs.js?version=1079637
+// @updateURL   https://raw.githubusercontent.com/chenghuang-mdsol/UserScriptsRepo/main/wider-gha-popup.js
+// @downloadURL https://raw.githubusercontent.com/chenghuang-mdsol/UserScriptsRepo/main/wider-gha-popup.js
+// @supportURL  https://github.com/Mottie/chenghuang-mdsol/UserScriptsRepo/issues
+// ==/UserScript==
+
+(function() {
+
+    function overwrite() {
+        GM_addStyle (`
+          .Popover-message--large {
+              min-width: 500px !important;
+          }
+          .SelectMenu-modal {
+              width: 480px !important
+          }
+     ` );
+	}
+    overwrite();
+})();
